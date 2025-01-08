565395d55d12083a38926da58e8a7fd4f17419d1
aeneidITHS <112550107+aeneidITHS@users.noreply.github.com>
2025-01-08 15:01:15 +0100

    Almost done with sorter, just some small stuff left hehehe

--- src/DogSorter.java
+++ src/DogSorter.java
@@ -1,8 +1,11 @@
 import java.util.ArrayList;
 import java.util.Comparator;
 
+
 public class DogSorter {
-    private static void swapDogs(ArrayList<Dog> dogs,int indexOne,int indexTwo) {
+    private static int swapCounter = 0;
+
+    public static void swapDogs(ArrayList<Dog> dogs,int indexOne,int indexTwo) {
         if(indexOne > dogs.size() || indexTwo > dogs.size()) {
             throw new ArrayIndexOutOfBoundsException("Index out of bounds");
         }
@@ -11,19 +14,37 @@ public class DogSorter {
 
         dogs.set(indexOne, dogTwo);
         dogs.set(indexTwo, dogOne);
+        System.out.println("Dog swapped at " + indexOne + " and " + indexTwo);
+
+        swapCounter++;
+
+
 
     }
 
-    private static int nextDog(Comparator<Dog> dogComparator,ArrayList<Dog> dogs,int startIndex) {
+    public static int nextDog(Comparator<Dog> dogComparator,ArrayList<Dog> dogs,int startIndex) {
         int size = dogs.size();
         int minIndex = startIndex;
-        for(int i = startIndex; i < size; i++ )  {
-            int comparison = dogComparator.compare(dogs.get(i),dogs.get(startIndex));
-                if(comparison < 0) {
-                    minIndex = i;
-                }
-
+        for (int i = startIndex; i <= size - 1; i++) {
+            if (dogComparator.compare(dogs.get(i), dogs.get(minIndex)) < 0) {
+                minIndex = i;
+            }
         }
         return minIndex;
+
+
     }
+
+    public static int sortDogs(Comparator<Dog> dogComparator,ArrayList<Dog> dogs) {
+        for (int i = 0; i < dogs.size(); i++) {
+            int minIndex = nextDog(dogComparator,dogs,i);
+            if(minIndex != i) {
+                swapDogs(dogs,minIndex,i);
+            }
+        }
+        return swapCounter;
+
+    }
+
+
 }
--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,25 @@
+import java.util.ArrayList;
+
 public class Main {
     public static void main(String[] args) {
+        Dog dog1 = new Dog("Steve","mops",1,10);
+        Dog dog2 = new Dog("Rari","tax",7,9);
+        Dog dog3 = new Dog("Lalla","bulldog",8,3);
+        Dog dog4 = new Dog("Illo","chiwaua",2,4);
+        Dog dog5 = new Dog("Steven","Great Dane",2,4);
 
+        DogSorter sorter = new DogSorter();
+        ArrayList<Dog> dogs = new ArrayList<Dog>();
+        DogTailNameComparator tailNameComparator = new DogTailNameComparator();
+        dogs.add(dog1);
+        dogs.add(dog2);
+        dogs.add(dog3);
+        dogs.add(dog4);
+        dogs.add(dog5);
+        System.out.println(DogSorter.sortDogs(tailNameComparator,dogs));
+        for (Dog dog : dogs) {
+            System.out.println(dog);
+        }
+        System.out.println("Index5" +dogs.get(4));
     }
 }
